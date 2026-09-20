12cb95c35b502f76d3e0423dcd55428e4a66e5b6
Tom X Nguyen <tom81094@gmail.com>
2026-09-20 15:36:13 +0700

    fix: support pi 0.86.0

--- extensions/pi-tps/__tests__/helpers.ts
+++ extensions/pi-tps/__tests__/helpers.ts
@@ -142,7 +142,7 @@ export function createTestFixture(): TestFixture {
   const mockPi: Partial<ExtensionAPI> = {
     on: vi.fn((event: string, handler: any) => {
       handlers[event] = handler;
-      return mockPi as ExtensionAPI;
+      return () => {};
     }),
     appendEntry: appendEntrySpy,
     registerCommand: registerCommandSpy,
